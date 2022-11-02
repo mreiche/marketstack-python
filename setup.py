ef23081ff14ca2dfd14935295fff0ddb77bf2c9c
Mike Reiche <mike.reiche@ewerk.com>
2022-11-02 22:28:11 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
     name="marketstack",
     version="0.1",
-    url="https://github.com/mreiche/marketstack",
+    url="https://github.com/mreiche/marketstack-python",
     author="Mike Reiche",
-    author_email="mike@reiche.world",
     py_modules=["marketstack"],
     install_requires=[],
     extra_test=[
